25efc4143415ec775e0c0143bedf572ecb9f241f
richard <richaled@163.com>
2022-01-20 11:53:44 +0800

    删除无用的libev库

---
+++ app/src/main/cpp/player2/video_render.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HFY on 2022/1/20.
+//
+
+#include "video_render.h"
---
+++ app/src/main/cpp/player2/video_render.h
@@ -0,0 +1,14 @@
+//
+// Created by HFY on 2022/1/20.
+//
+
+#ifndef FFMPEGDEMO_VIDEO_RENDER_H
+#define FFMPEGDEMO_VIDEO_RENDER_H
+
+
+class video_render {
+
+};
+
+
+#endif //FFMPEGDEMO_VIDEO_RENDER_H
